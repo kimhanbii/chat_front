84c910de9f7a826355eca87b6276bc49c3263822
khb <stellar0109@naver.com>
2024-01-19 13:04:57 +0900

    chat_front

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-import {useEffect, useRef, useState} from 'react'
+import {useEffect, useRef, useState, useSyncExternalStore} from 'react'
 import server from './server';
+import { flushSync } from 'react-dom';
+import userEvent from '@testing-library/user-event';
 
 
 function App() {
 
   const [chat, setChat] = useState(false)
+  const [nickName, setNickName] = useState("") // 로그인시 닉네임 저장
   const [msg, setMsg] = useState([])
   
   const input_ref=useRef("")
   const msg_ref=useRef("")
   const scroll_ref=useRef(0)
+  // const nickname_ref=useRef("")
 
   function handleClick(){
     server.emit('login', input_ref.current.value)
     setChat(true)
+  }
+
+  function handleClick(){
+    server.emit('login', input_ref.current.value)
+    setChat(true)
+    setNickName(input_ref.current.value)
   }
 
   function sendClick(){
@@ -24,7 +34,14 @@ function App() {
       let msgArray = [...msg]
       msgArray.push({level:"me", msg:msg_ref.current.value})
       setMsg(msgArray)
-      server.emit('send', msg_ref.current.value)
+      server.emit('send', {nickName:nickName, msg:msg_ref.current.value }) // 메세지 전송시 닉네임 전송
+    }
+  }
+
+  //메세지 전송시 엔터클릭
+  function sendKeyClick(e){
+    if(e.key == 'Enter'){
+      sendClick()
     }
   }
   
@@ -37,9 +54,12 @@ function App() {
       msgArray.push(data)
       setMsg(msgArray)
     })
-    if(msg_ref.current){ 
+
+    //메세지 전송후 인풋박스 클리어 + 나의 작성 메세지만 클리어
+    if(msg_ref.current && msg[msg.length -1].level == "me"){ 
       msg_ref.current.value = ""
     }
+
   },[msg])
 
 
@@ -62,6 +82,7 @@ function App() {
               return <div className='chat_boxox'
                 style={{
                   justifyContent: c.level == "sys" ? "center" : c.level == "" ? "start" : "end" }}>
+                  <div className='nickname_style'>{c.nickName}</div> {/* 메세지 보낸 사람 닉네임 표시 */}
                   <div className={c.level == "sys" ? 'chat_box_center' : "chat_box"}>{c.msg}</div>
                 </div>
             })
@@ -69,7 +90,7 @@ function App() {
           </div>
 
         <div>
-          <input ref={msg_ref}></input>
+          <input ref={msg_ref} onKeyPress={sendKeyClick}></input> {/*메세지 엔터클릭*/}
           <button onClick={sendClick}>send</button>
         </div>
 
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
 import {io} from 'socket.io-client'
-const server = io('https://chat-server-lris.onrender.com')
+const server = io('http://localhost:3001')
+// const server = io('https://chat-server-lris.onrender.com')
 export default server
