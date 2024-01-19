437dad5dbb203523f9451990aba0a594d4418e53
khb <stellar0109@naver.com>
2024-01-19 13:46:46 +0900

    chat_front

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
 import {io} from 'socket.io-client'
-const server = io('http://localhost:3001')
-// const server = io('https://chat-server-lris.onrender.com')
+// const server = io('http://localhost:3001')
+const server = io('https://chat-front-8jnk.onrender.com')
 export default server
